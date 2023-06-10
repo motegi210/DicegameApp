9c50eb01e6ff7f62e4735daabcdea990338b216e
motegi210 <pandemohr@gmail.com>
2023-06-10 16:50:49 +0900

    Won/Lost message added

--- DiceApplication.java
+++ DiceApplication.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 class DiceApplication{
     public static void main(String[] args){
+        System.out.println("What is your name?");
+        System.out.print("> ");
+        Scanner sc1 = new Scanner(System.in);
+        String name = sc1.next();
+        System.out.println("Hello, " + name + "!");
         System.out.println("Rolling dice...");
         System.out.print("Die 1:");
         Scanner sc2 = new Scanner(System.in);
@@ -10,5 +15,9 @@ class DiceApplication{
         int n2 = sc2.nextInt();
         int sum = n1 + n2;
         System.out.println("Total value: " + sum);
+        if(sum > 7){
+            System.out.println("You won");
+        }
+        else System.out.println("You lost");
     }
 }
