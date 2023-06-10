4a951057ee4112fc8e1823fc3e488ef633b75943
motegi210 <pandemohr@gmail.com>
2023-06-10 16:44:30 +0900

    Greetings added

--- DiceApplication.java
+++ DiceApplication.java
@@ -2,11 +2,16 @@ import java.util.Scanner;
 
 class DiceApplication{
     public static void main(String[] args){
+        System.out.println("What is your name?");
+        System.out.print("> ");
+        Scanner sc1 = new Scanner(System.in);
+        String name = sc1.next();
+        System.out.println("Hello, " + name + "!");
         System.out.println("Rolling dice...");
-        System.out.print("Die 1:");
+        System.out.print("die 1:");
         Scanner sc2 = new Scanner(System.in);
         int n1 = sc2.nextInt();
-        System.out.print("Die 2:");
+        System.out.print("die 2:");
         int n2 = sc2.nextInt();
         int sum = n1 + n2;
         System.out.println("Total value: " + sum);
